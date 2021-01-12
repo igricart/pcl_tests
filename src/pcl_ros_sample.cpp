b3ee2f45b2be02aff744030c90f91f8cefa9f1eb
Ignacio Ricart <ignacio@poli.ufrj.br>
2021-01-12 23:48:19 +0100

    add more callbacks and publish different msg type

--- src/pcl_ros_sample.cpp
+++ src/pcl_ros_sample.cpp
@@ -5,11 +5,65 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
 
 ros::Publisher pub;
 
-void 
-cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
+void cloud_cb_model_plane (const sensor_msgs::PointCloud2ConstPtr& input)
+{
+  // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
+  pcl::PointCloud<pcl::PointXYZ> cloud;
+  pcl::fromROSMsg (*input, cloud);
+
+  pcl::ModelCoefficients coefficients;
+  pcl::PointIndices inliers;
+  // Create the segmentation object
+  pcl::SACSegmentation<pcl::PointXYZ> seg;
+  // Optional
+  seg.setOptimizeCoefficients (true);
+  // Mandatory
+  seg.setModelType (pcl::SACMODEL_PLANE);
+  seg.setMethodType (pcl::SAC_RANSAC);
+  seg.setDistanceThreshold (0.01);
+
+  seg.setInputCloud (cloud.makeShared ());
+  seg.segment (inliers, coefficients);
+
+  // Publish the model coefficients
+  pcl_msgs::ModelCoefficients ros_coefficients;
+  pcl_conversions::fromPCL(coefficients, ros_coefficients);
+  pub.publish (ros_coefficients);
+}
+
+void cloud_cb_model_cylinder (const sensor_msgs::PointCloud2ConstPtr& input)
+{
+  // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
+  pcl::PointCloud<pcl::PointXYZ> cloud;
+  pcl::fromROSMsg (*input, cloud);
+
+  pcl::ModelCoefficients coefficients;
+  pcl::PointIndices inliers;
+  // Create the segmentation object
+  pcl::SACSegmentation<pcl::PointXYZ> seg;
+  // Optional
+  seg.setOptimizeCoefficients (true);
+  // Mandatory
+  seg.setModelType (pcl::SACMODEL_CYLINDER);
+  seg.setMethodType (pcl::SAC_RANSAC);
+  seg.setDistanceThreshold (0.01);
+
+  seg.setInputCloud (cloud.makeShared ());
+  seg.segment (inliers, coefficients);
+
+  // Publish the model coefficients
+  pcl_msgs::ModelCoefficients ros_coefficients;
+  pcl_conversions::fromPCL(coefficients, ros_coefficients);
+  pub.publish (ros_coefficients);
+}
+
+void cloud_cb_do_nothing (const sensor_msgs::PointCloud2ConstPtr& input)
 {
   // Create a container for the data.
   sensor_msgs::PointCloud2 output;
@@ -21,18 +75,44 @@ cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
   pub.publish (output);
 }
 
-int
-main (int argc, char** argv)
+void cloud_cb_filter_voxel_grid (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
+{
+  // Container for original & filtered data
+  pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
+  pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
+  pcl::PCLPointCloud2 cloud_filtered;
+
+  // Convert to PCL data type
+  pcl_conversions::toPCL(*cloud_msg, *cloud);
+
+  // Perform the actual filtering
+  pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+  sor.setInputCloud (cloudPtr);
+  sor.setLeafSize (0.1, 0.1, 0.1);
+  sor.filter (cloud_filtered);
+
+  // Convert to ROS data type
+  sensor_msgs::PointCloud2 output;
+  pcl_conversions::fromPCL(cloud_filtered, output);
+
+  // Publish the data
+  pub.publish (output);
+}
+
+int main (int argc, char** argv)
 {
   // Initialize ROS
   ros::init (argc, argv, "my_pcl_tutorial");
   ros::NodeHandle nh;
 
   // Create a ROS subscriber for the input point cloud
-  ros::Subscriber sub = nh.subscribe ("/velodyne_points2", 1, cloud_cb);
+  ros::Subscriber sub = nh.subscribe ("input", 1, cloud_cb_model_cylinder);
 
   // Create a ROS publisher for the output point cloud
-  pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
+  // pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
+
+  // Create a ROS publisher for the output model coefficients
+  pub = nh.advertise<pcl_msgs::ModelCoefficients> ("output", 1);
 
   // Spin
   ros::spin ();
